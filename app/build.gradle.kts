c866fb223157fed62d2f4c577fd6d7f411f21bae
ashMatch <bruno.rochah95@gmail.com>
2024-09-13 00:33:02 -0300

    feat: height and weight project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.material:material")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
@@ -66,4 +67,6 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+
 }
--- app/src/main/java/com/jellyfish/composeproject/MainActivity.kt
+++ app/src/main/java/com/jellyfish/composeproject/MainActivity.kt
@@ -25,74 +25,83 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.jellyfish.composeproject.ui.theme.ComposeProjectTheme
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row {
-                MyScreen()
-                LocalImageExample()
-                Counter()
+            MassCalculatorApp()
+        }
+    }
 
+    @Composable
+    private fun MassCalculatorApp() {
+        var heightInput by remember { mutableStateOf("") }
+        var weightInput by remember { mutableStateOf("") }
+        var result by remember { mutableStateOf<Int?>(null) }
+        var isSubmitted by remember { mutableStateOf(false) }
+
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            if (!isSubmitted){
+                // Inputs para altura e peso
+                BasicTextField(
+                    value = heightInput,
+                    onValueChange = {heightInput = it},
+                    textStyle = TextStyle(fontSize = 18.sp, color = Color.Black),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                )
+                BasicTextField(
+                    value = weightInput,
+                    onValueChange = {weightInput = it},
+                    textStyle = TextStyle(fontSize = 18.sp, color = Color.Black),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                )
+
+                Button(onClick = {
+                    // Conversão dos valores de entrada e cálculo da massa
+                    val height = heightInput.toIntOrNull() ?: 0
+                    val weight = weightInput.toIntOrNull() ?: 0
+                    result = height*weight
+                    isSubmitted = true
+                }) {
+                    Text("Calcular")
+
+                }
+            } else {
+                // Exibindo os valores inseridos e o resultado do cálculo
+                Text("Altura: $heightInput", fontSize = 20.sp)
+                Text("Peso: $weightInput", fontSize = 20.sp)
+                Text("Massa: ${result ?: 0}", fontSize = 24.sp, color = Color.Blue)
+
+                // Exibição da imagem logo abaixo do resultado
+                Image(
+                    painter = painterResource(id = R.drawable.inuyasha),
+                    contentDescription =null,
+                    modifier = Modifier
+                        .padding(top = 16.dp)
+                        .size(150.dp)
+                )
             }
         }
+
     }
 }
-
-@Composable
-fun MessageCard(name:String){
-    Text(text = "Olá $name")
-
-}
-
-@Composable
-fun MyScreen(){
-    Column {
-        MessageCard("Ladino")
-        MessageCard("Mago")
-    }
-}
-
-@Composable
-fun Counter(){
-    var count by remember { mutableStateOf(0) }
-
-    Column {
-        Text(text = "Contador: $count")
-        Button(
-            onClick = { count++},
-            modifier = Modifier.padding(50.dp)
-            ) {
-            Text(text = "Aumentar")
-        }
-    }
-}
-
-
-@Composable
-fun LocalImageExample(){
-    Image(
-        painter = painterResource(id = R.drawable.inuyasha),
-        contentDescription = null
-    )
-}
-
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Fala tu $name!",
-//        modifier = modifier
-//    )
-//}
-
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    ComposeProjectTheme {
-//        Greeting("Vagabundo")
-//    }
-//}
